18f928af4c5a4787235466708e671c6e7eb54ef9
Jonathan <mbjonathan@misena.edu.co>
2021-07-02 23:47:47 -0500

    Retos Mintic Ciclo 1 - Python

---
+++ reto4.py
@@ -0,0 +1,30 @@
+import json
+
+lista=input()
+listaj=json.loads(lista)
+ingredientes=input()
+ingredientes=ingredientes.split(" ")
+ingredientes=list(ingredientes)
+
+i=0
+total=0
+ingredientesTotal=[]
+ingredientesOrden=[]
+
+for key in listaj.keys():
+    for i in range(len(ingredientes)):
+      if ingredientes[i] in key:
+          total=total+listaj[key]
+          ingredientesTotal.append(key)
+          i=i+1
+
+    i=0
+
+for c in ingredientes:
+  if c in ingredientesTotal:
+    ingredientesOrden.append(c)          
+    
+  
+ingredientesOrden=" ".join(map(str,ingredientesOrden))
+print(total)
+print(ingredientesOrden)
